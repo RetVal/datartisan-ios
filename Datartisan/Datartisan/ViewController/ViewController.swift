04930879a3706f3288f2aaa2af40ec0c1ce75d00
郑峰涛 <Color@zhengfengtaodeMacBook-Pro.local>
2015-08-18 08:02:40 +0800

    添加设置功能

--- Datartisan/Datartisan/ViewController/ViewController.swift
+++ Datartisan/Datartisan/ViewController/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         self.view.backgroundColor = UIColor.blueColor()
+        self.title = "文章列表";
+        
+        self.navigationController?.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "设置", style: .Plain, target: self, action: "showSetting")
+    }
+    
+    func showSetting(sender:UIBarButtonItem) {
     }
 
     override func didReceiveMemoryWarning() {
